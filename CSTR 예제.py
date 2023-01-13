fee5e6464b099f913df877fc75b8e579011b8a4a
Kwan Woo Park <kwanwoopark97@gmail.com>
2023-01-13 19:27:32 +0900

    오류있으면 말해주세요

--- CSTR 예제.py
+++ CSTR 예제.py
@@ -1,19 +1,36 @@
-import math
 import tensorflow as tf
 import numpy as np
+import math
+import cvxpy as cp
+def plant(y,t,u):
+    #input u= w1  output cbt,ht
+    w2, cb1, cb2, k1, k2 =0.1 , 24.9, 0.1, 1.0,1.0
+    dhdt=u+w2-0.2*math.sqrt(y[0])
+    dcbdt=(cb1-y[1])*(u/y[0])+(cb2-y[1])*(w2/y[0])-(k1*y[1])/((1+k2*y[1])**2)
 
+    output=[dhdt,dcbdt]
 
+    return output
 
-class MPC_plant(object):
+def mpc_model(ref,x,u):
+    n = 2
+    m = 2
+    T = 2
 
-    def __init__(self):
-        self.h_t=30
-        self.w2=0.1
-        self.cb1=24.9
-        self.cb2=0.1
-        self.k1=1.0
-        self.k2=1.0
+    A =np.array([-5,-0.3427,47.68,2.785]).reshape(2,2)
+    B = np.array([0.0,1.0,0.3,0.0]).reshape(2,2)
+    x = cp.Variable((n, T + 1))
+    u = cp.Variable((m, T))
 
+    cost = 0
+    constr = []
+    for t in range(T):
+        cost += cp.sum_squares(x[:, t + 1]) + cp.sum_squares(u[:, t])
+        constr += [x[:, t + 1] == A @ x[:, t] + B @ u[:, t], cp.norm(u[:, t], "inf") <= 1]
+    # sums problem objectives and concatenates constraints.
+    constr += [x[:, T] == 0, x[:, 0] == x_0]
+    problem = cp.Problem(cp.Minimize(cost), constr)
+    problem.solve()
 
 
 
