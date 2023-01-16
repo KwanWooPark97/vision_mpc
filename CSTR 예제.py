7a61249fc306482faf49410cee5b6e06425effd2
Kwan Woo Park <kwanwoopark97@gmail.com>
2023-01-16 19:29:58 +0900

    오류있으면 말해주세요

--- CSTR 예제.py
+++ CSTR 예제.py
@@ -1,7 +1,9 @@
-import tensorflow as tf
+#import tensorflow as tf
 import numpy as np
 import math
 import cvxpy as cp
+import matplotlib.pyplot as plt
+
 def plant(y,t,u):
     #input u= w1  output cbt,ht
     w2, cb1, cb2, k1, k2 =0.1 , 24.9, 0.1, 1.0,1.0
@@ -12,28 +14,53 @@ def plant(y,t,u):
 
     return output
 
-def mpc_model(ref,x,u):
+def mpc_model(x_ref):
     n = 2
     m = 2
-    T = 2
+    T = 20
 
-    A =np.array([-5,-0.3427,47.68,2.785]).reshape(2,2)
+    A =np.array([-5.0,-0.3427,47.68,2.785]).reshape(2,2)
     B = np.array([0.0,1.0,0.3,0.0]).reshape(2,2)
     x = cp.Variable((n, T + 1))
     u = cp.Variable((m, T))
-
+    x_0=np.array([8.5698,311.2639])
     cost = 0
     constr = []
+
     for t in range(T):
-        cost += cp.sum_squares(x[:, t + 1]) + cp.sum_squares(u[:, t])
-        constr += [x[:, t + 1] == A @ x[:, t] + B @ u[:, t], cp.norm(u[:, t], "inf") <= 1]
+        cost += cp.sum_squares(x_ref-x[:, t+1]) + cp.sum_squares(u[:, t])
+        constr += [x[:, t + 1] == A @ x[:, t] + B @ u[:, t]]
+
     # sums problem objectives and concatenates constraints.
-    constr += [x[:, T] == 0, x[:, 0] == x_0]
+    constr += [x[:, 0] == x_0]
+    constr += [x[0,:] <= 10.0]
+    constr += [x[0, :] >= 0.0]
+    constr += [x[1, :] <= 390.0]
+    constr += [x[1, :] >= 310.0]
     problem = cp.Problem(cp.Minimize(cost), constr)
     problem.solve()
+    print(problem.status)
+    print(x.value)
+    print(u.value)
 
+    rx0=np.array(x.value[0, :]).flatten()
+    rx1=np.array(x.value[1,:]).flatten()
+    u1=np.array(u.value[0,:]).flatten()
+    u2=np.array(u.value[1,:]).flatten()
+    plt.subplot(2,2,1)
+    plt.title('state_0')
+    plt.plot(rx0)
+    plt.subplot(2,2,2)
+    plt.title('state_1')
+    plt.plot(rx1)
+    plt.subplot(2, 2, 3)
+    plt.title('input_0')
+    plt.plot(u1)
+    plt.subplot(2, 2, 4)
+    plt.title('input_1')
+    plt.plot(u2)
 
-
+    plt.show()
 
 
 
@@ -42,7 +69,7 @@ def mpc_model(ref,x,u):
 
 
 if __name__ == '__main__':
-    gpus = tf.config.experimental.list_physical_devices('GPU')
+    '''gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
         try:
             # Currently, memory growth needs to be the same across GPUs
@@ -52,4 +79,5 @@ if __name__ == '__main__':
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
-            print(e)
+            print(e)'''
+    mpc_model(np.array([8.0,350.0]))
