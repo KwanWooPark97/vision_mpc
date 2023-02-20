758d248a22851d487ed2049c77af58fe1dd304c1
Kwan Woo Park <kwanwoopark97@gmail.com>
2023-02-20 19:27:22 +0900

    오류있으면 말해주세요

---
+++ MPC_test.py
@@ -0,0 +1,180 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import LSTM, Dense
+from collections import deque
+from cartpole_gym_env import CartPoleEnv
+import matplotlib.pyplot as plt
+from scipy import optimize
+
+'''gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+class LSTM_test(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.s1 = LSTM(512, return_sequences=True)
+        self.s2 = LSTM(256, return_sequences=True)
+        self.s3 = LSTM(128)
+        self.s4 = Dense(4)
+
+    def call(self, states):
+        features1 = self.s1(states)
+        features1 = self.s2(features1)
+        features1 = self.s3(features1)
+        features1 = self.s4(features1)
+
+        return features1'''
+
+# Define the plant model
+#plant = LSTM_test()
+#plant.load_weights('sample_model3d2')
+# Define the control horizon and the prediction horizon
+control_horizon = 3
+prediction_horizon = 70
+
+# Define the state and control bounds
+state_bounds = np.array([[-100, 100], [-100, 100], [-3*np.pi, 3*np.pi], [-100, 100]])
+control_bounds = (-30,30)
+bnd=((-30.0,30.0),)
+# Define the initial state and setpoint
+initial_state = np.array([[0, 0, np.pi, 0]])
+setpoint = np.array([[0, 0, 0, 0]])
+
+def mpc_controller(initial_state, initial_force,setpoint, control_horizon, prediction_horizon, state_bounds, bnd):
+    # Define the cost function
+    def cost_function(state, control):
+        state_error = np.array(state)
+        control_error = control
+        cost = 0.1*state_error[0]**2+0.1*state_error[1]**2+state_error[2]**2+state_error[3]**2 + control_error**2
+        return cost
+
+    force=np.array(initial_force).reshape(1,prediction_horizon)
+    # Define the MPC loop
+    #force = deque(np.array(initial_force),maxlen=10)
+    #force=np.zeros(10)
+    state_buffer = deque(np.array(initial_state),maxlen=10)
+
+        # Define the optimization problem
+    def optimization_problem(control):
+        x_pred=np.zeros((4,prediction_horizon+1))
+        x_pred[:,0]=state_buffer[-1]
+        cost = 0
+        #state_buffer = deque(np.array(state),maxlen=10)
+        #force_buffer= deque(np.array(force),maxlen=10)
+        #control=np.reshape(control,(10,1))
+        for i in range(prediction_horizon):
+            x_pred[:,i+1]=env.test_step(x_pred[:,i],control[i])
+        for j in range(prediction_horizon):
+            cost += cost_function(x_pred[:,j+1], control[j])
+
+        return cost
+
+    def constraint_function(u):
+        return 1 - np.linalg.norm(u, ord=np.inf)
+    bound=tuple((-30,30) for _ in range(prediction_horizon))
+    con = {'type': 'ineq', 'fun': constraint_function}
+    # Solve the optimization problem
+    result = optimize.minimize(optimization_problem, force, bounds=bound)
+    '''force.append(result.x)
+    # Apply the control
+    next_state = plant(np.concatenate((state, force), axis=1).reshape([1,10,5]))[0]
+    state.append(next_state)'''
+
+    return result.x[0]
+
+# Call the MPC controller
+env=CartPoleEnv("human")
+state,_=env.reset()
+plt.figure(figsize=(8,5))
+plt.ion()
+plt.show()
+plot_t=[]
+force=np.array([0.0])
+
+#state=np.append(state,force)
+plot_x_hat=[]
+plot_theta_hat=[]
+state_deq = deque([np.zeros_like(state) for _ in range(10)],maxlen=10)
+state_deq.append(state)
+state_deq_mpc = deque([np.zeros_like(state) for _ in range(10)],maxlen=10)
+state_deq_mpc.append(state)
+force_deq=deque([np.zeros_like(force) for _ in range(prediction_horizon)],maxlen=prediction_horizon)
+force_deq_mpc=deque([np.zeros_like(force) for _ in range(prediction_horizon)],maxlen=prediction_horizon)
+
+plot_force=[]
+plot_x=[]
+plot_theta=[]
+t=0
+# Apply the MPC control
+######################################################################################
+'''# initial conditions
+pi=np.pi
+Y = .0  # pendulum angular velocity
+th = pi - 0.1  # pendulum angle
+x = .0  # cart position
+x0 = 0  # desired cart position
+Z = -0.05  # cart velocity
+
+state = np.array([th, Y, x, Z])'''
+while True:
+    state_deq_mpc=state_deq.copy()
+    force_deq_mpc=force_deq.copy()
+    control = mpc_controller(state_deq_mpc, force_deq_mpc, setpoint, control_horizon, prediction_horizon, state_bounds, bnd)
+    force = control
+    force= np.reshape(force,(1,))
+    force_deq.append(force)
+    #x = np.concatenate((state_deq,force_deq),axis=1).reshape([1, 10, 5])
+    #cart_position_hat, cart_position_dot_hat, theta_real_hat, theta_dot_real_hat = plant.predict(x)[0]
+
+    cart_position, cart_position_dot, theta_real, theta_dot_real = env.step(force[0])
+
+
+
+    next_state = np.array([cart_position, cart_position_dot, theta_real, theta_dot_real])
+
+
+
+    #state = np.append(next_state, force)
+    state_deq.append(next_state)
+
+    plot_x.append(cart_position)
+    #plot_x_hat.append(cart_position_hat)
+    plot_theta.append(theta_real)
+    #plot_theta_hat.append(theta_real_hat)
+    plot_force.append(force)
+    plot_t.append(t)
+    t += 1
+
+    plt.clf()
+    plt.subplot(3, 1, 1)
+    plt.plot(plot_t, plot_force, 'b--', lw=3)
+    plt.ylabel('input Force')
+    plt.legend(['force'], loc='best')
+
+    plt.subplot(3, 1, 2)
+    plt.plot(plot_t, plot_x, 'b.-', lw=3, label=r'$position$')
+    #plt.plot(plot_t, plot_x_hat, 'g-', lw=3, label=r'$NN$')
+    plt.axhline(0.0, 0.1, 0.9, color='r', linestyle='-', label=r'$position_{sp}$')
+    plt.ylabel(r'cart position')
+    plt.legend(loc='best')
+
+    plt.subplot(3, 1, 3)
+    #plt.plot(plot_t, plot_theta_hat, 'g-', lw=3, label=r'$NN$')
+    plt.axhline(0.0, 0.1, 0.9, color='r', linestyle='-', label=r'$theta_{sp}$')
+    plt.plot(plot_t, plot_theta, 'b.-', lw=3, label=r'$theta_{meas}$')
+    plt.ylabel('theta')
+    plt.xlabel('Time (min)')
+    plt.legend(loc='best')
+    plt.draw()
+    plt.pause(0.01)
+
+
--- cartpole_gym_env.py
+++ cartpole_gym_env.py
@@ -1,12 +1,12 @@
 import math
 from typing import Optional, Union
-#from scipy.integrate import odeint
+from scipy.integrate import odeint
 import numpy as np
 
 import gym
 from gym import logger, spaces
 from gym.error import DependencyNotInstalled
-
+from math import cos,sin
 """
 Utility functions used for classic control environments.
 """
@@ -176,9 +176,9 @@ class CartPoleEnv(gym.Env):
         # Return xdot:
         xdot = np.zeros(4)
         xdot[3] = thetaacc
-        xdot[2] = xdot[3]
+        xdot[2] = state[3]
         xdot[1] = xacc
-        xdot[0] = xdot[1]
+        xdot[0] = state[1]
 
         return xdot
 
@@ -197,7 +197,7 @@ class CartPoleEnv(gym.Env):
         self.state[3]= y[-1][3]
         #force = self.force_mag if action == 1 else -self.force_mag
         self.time+=0.08'''
-        '''costheta = math.cos(theta)
+        costheta = math.cos(theta)
         sintheta = math.sin(theta)
 
         # For the interested reader:
@@ -206,16 +206,16 @@ class CartPoleEnv(gym.Env):
         thetaacc = (self.gravity * sintheta - costheta * temp) / (
                 self.length * (4.0 / 3.0 - self.masspole * costheta ** 2 / self.total_mass)
         )
-        xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass'''
+        xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
 
-        Kd=10
+        '''Kd=10
         z_dot=x_dot
         mPend=1.0
         L=0.5
         g=9.81
         mCart=1.0
         xacc=(force - Kd*z_dot - mPend*L*theta_dot**2*math.sin(theta) + mPend*g*math.sin(theta)*math.cos(theta)) / (mCart + mPend*math.sin(theta)**2)
-        thetaacc=((force - Kd*z_dot - mPend*L*theta_dot**2*math.sin(theta))*math.cos(theta)/(mCart + mPend) + g*math.sin(theta)) / (L - mPend*L*math.cos(theta)**2/(mCart + mPend))
+        thetaacc=((force - Kd*z_dot - mPend*L*theta_dot**2*math.sin(theta))*math.cos(theta)/(mCart + mPend) + g*math.sin(theta)) / (L - mPend*L*math.cos(theta)**2/(mCart + mPend))'''
 
 
         if self.kinematics_integrator == "euler":
@@ -261,6 +261,135 @@ class CartPoleEnv(gym.Env):
         if self.render_mode == "human":
             self.render()
         return np.array(self.state, dtype=np.float32)#, reward, terminated, False, {}
+    def test_step(self, test,action):
+        #err_msg = f"{action!r} ({type(action)}) invalid"
+        #assert self.action_space.contains(action), err_msg
+        #assert self.state is not None, "Call reset before using step method."
+        force = action
+
+        x, x_dot, theta, theta_dot = test
+        '''ts = np.array([self.time+i*self.tau for i in range(50)])
+        y = odeint(self.pendulum, test, ts, args=(force,))
+        # retrieve measurements
+        test[0]= y[-1][0]
+        test[1]= y[-1][1]
+        test[2]= y[-1][2]
+        test[3]= y[-1][3]
+        #force = self.force_mag if action == 1 else -self.force_mag
+        self.time+=self.tau'''
+        costheta = math.cos(theta)
+        sintheta = math.sin(theta)
+
+        # For the interested reader:
+        # https://coneural.org/florian/papers/05_cart_pole.pdf
+        temp = ( force + self.polemass_length * theta_dot ** 2 * sintheta) / self.total_mass
+        thetaacc = (self.gravity * sintheta - costheta * temp) / (
+                self.length * (4.0 / 3.0 - self.masspole * costheta ** 2 / self.total_mass)
+        )
+        xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
+
+        '''Kd=10
+        z_dot=x_dot
+        mPend=1.0
+        L=0.5
+        g=9.81
+        mCart=1.0
+        xacc=(force - Kd*z_dot - mPend*L*theta_dot**2*math.sin(theta) + mPend*g*math.sin(theta)*math.cos(theta)) / (mCart + mPend*math.sin(theta)**2)
+        thetaacc=((force - Kd*z_dot - mPend*L*theta_dot**2*math.sin(theta))*math.cos(theta)/(mCart + mPend) + g*math.sin(theta)) / (L - mPend*L*math.cos(theta)**2/(mCart + mPend))'''
+
+
+        if self.kinematics_integrator == "euler":
+            x = x + self.tau * x_dot
+            x_dot = x_dot + self.tau * xacc
+            theta = theta + self.tau * theta_dot
+            theta_dot = theta_dot + self.tau * thetaacc
+        else:  # semi-implicit euler
+            x_dot = x_dot + self.tau * xacc
+            x = x + self.tau * x_dot
+            theta_dot = theta_dot + self.tau * thetaacc
+            theta = theta + self.tau * theta_dot
+        if theta >= 2*math.pi:
+            theta=theta%(2*math.pi)
+        elif theta <= -2*math.pi:
+            theta=theta%(2*math.pi)
+        test = (x, x_dot, theta, theta_dot)
+
+        '''terminated = bool(
+            x < -self.x_threshold
+            or x > self.x_threshold
+            or theta < -self.theta_threshold_radians
+            or theta > self.theta_threshold_radians
+        )'''
+
+        '''if not terminated:
+            reward = 1.0
+        elif self.steps_beyond_terminated is None:
+            # Pole just fell!
+            self.steps_beyond_terminated = 0
+            reward = 1.0
+        else:
+            if self.steps_beyond_terminated == 0:
+                logger.warn(
+                    "You are calling 'step()' even though this "
+                    "environment has already returned terminated = True. You "
+                    "should always call 'reset()' once you receive 'terminated = "
+                    "True' -- any further steps are undefined behavior."
+                )
+            self.steps_beyond_terminated += 1
+            reward = 0.0'''
+
+        #if self.render_mode == "human":
+        #    self.render()
+        return np.array(test, dtype=np.float32)#, reward, terminated, False, {}
+
+    def test_step_1(self,state):
+        # simulation time
+        dt = 0.1
+        Tmax = 5
+        t = np.arange(0.0, Tmax, dt)
+        pi=np.pi
+        g = 9.8
+        L = 1.0
+        m = 0.5
+        # Controller coefficients
+        Kp_th = 50
+        Kd_th = 15
+        Kp_x = 3.1
+        Kd_x = 4.8
+        stabilizing = False
+
+        def energy(th, dth):
+            return m * dth * L * dth * L / 2 + m * g * L * (cos(th) - 1)
+
+        def isControllable(th, dth):
+            return th < pi / 9 and abs(energy(th, dth)) < 0.5
+
+        def derivatives(state, t):
+            global stabilizing
+            ds = np.zeros_like(state)
+
+            _th = state[0]
+            _Y = state[1]  # th'
+            _x = state[2]
+            _Z = state[3]  # x'
+            k = 0.08  # control gain coefficient
+            if stabilizing or isControllable(_th, _Y):
+                stabilizing = True
+                u = Kp_th * _th + Kd_th * _Y + Kp_x * _x + Kd_x * _Z
+            else:
+                E = energy(_th, _Y)
+                u = k * E * _Y * cos(_th)
+
+            ds[0] = state[1]
+            ds[1] = (g * sin(_th) - u * cos(_th)) / L
+            ds[2] = state[3]
+            ds[3] = u
+
+            return ds
+
+        solution = odeint(derivatives, state, t)
+
+        return solution
 
     def reset(
             self,
@@ -274,7 +403,7 @@ class CartPoleEnv(gym.Env):
         low, high = maybe_parse_reset_bounds(
             options, -0.05, 0.05  # default low
         )  # default high
-        self.state = [0,0,-3.14,0]
+        self.state = [0,0,3.14,0]
         self.steps_beyond_terminated = None
 
         if self.render_mode == "human":
